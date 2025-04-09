8a00b4eee09247ce2c5ca66010ced98fe552fd5f
Shendrea2 <134798170+Shendrea2@users.noreply.github.com>
2025-04-09 05:29:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Welcome to 149FM Radio!");
+<script src="https://cdn.jsdelivr.net/npm/howler@2.2.0/dist/howler.min.js"></script>
+<script>
+    var sound = new Howl({
+        src: ['http://91.99.52.93/listen/149fm/radio.mp3']
+    });
+    sound.play();
+</script>
