95be04a0473b39ebf1ac669a03b143fddfa18621
Shendrea2 <134798170+Shendrea2@users.noreply.github.com>
2025-04-09 05:58:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Welcome to 149FM Radio!");
 <script src="https://cdn.jsdelivr.net/npm/howler@2.2.0/dist/howler.min.js"></script>
 <script>
     var sound = new Howl({
-        src: ['http://91.99.52.93/listen/149fm/radio.mp3']
+        src: ['https://stream.zeno.fm/6vc4ddpr3ehvv']
     });
     sound.play();
 </script>
